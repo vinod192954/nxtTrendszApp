8a104d2a060e44d9b506cdfaf8fb97e3169d8ac6
vinod192954 <vvinnu590@gmail.com>
2024-08-29 20:42:25 +0530

    added home and login components and applied login functionalities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import Home from './components/Home';
+import LoginForm from './components/LoginForm';
 function App() {
   return (
     <div className="App">
-    <Home/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={LoginForm} />
+        <Route exact path="/home" component={Home}/>
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,71 @@
+import { useState } from "react"
+import Cookies from 'js-cookie'
+import "./index.css"
+const LoginForm=(props)=>{
+    const [username,setUserName] = useState('')
+    const [password,setPassword] = useState('')
+
+    const onChangeUsername=(event)=>{
+        setUserName(event.target.value)
+    }
+    const onChangePassword=(event)=>{
+        setPassword(event.target.value)
+    }
+
+    const authenticateSuccessfully=(jwtToken)=>{
+        Cookies.set("jwt_token",jwtToken) 
+        const {history} = props 
+        history.replace('/home')
+    }
+
+    const onSubmitUserDetais=async(event)=>{
+        event.preventDefault()
+        const userDetails = {username,password} 
+        const url = "https://apis.ccbp.in/login"
+        const options={
+            method:"POST",
+            body:JSON.stringify(userDetails),
+        }
+        const response = await fetch(url,options)
+        const data = await response.json()
+      if (response.ok===true){
+         const {jwt_token} = data
+         authenticateSuccessfully(jwt_token)
+      }
+
+      setUserName('')
+      setPassword('')
+    }
+
+    return (
+        <div className="Login-page">
+            <div className="login-image-container">
+                <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png" 
+                alt="website login" className="website-login"/>
+            </div>
+            <div className="right-side-section">
+            <div className="login-form-container">
+                <div>
+                    <img src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+                    alt="website logo"
+                    className="website-logo"/>
+                 </div>
+                 <form onSubmit={onSubmitUserDetais} className="login-card">
+                    <div>
+                        <label className="Name">USERNAME</label>
+                        <br/>
+                        <input value={username} onChange={onChangeUsername} className="user-input" type="text"placeholder="Username" />
+                    </div>
+                    <div>
+                        <label className="Name">PASSWORD</label>
+                        <br/>
+                        <input value={password} onChange={onChangePassword} className="user-input" type="password"placeholder="Password" />
+                    </div>
+                    <button  className="submit-button" type="submit">Login</button>
+                 </form>
+            </div>
+            </div>
+        </div>
+    )
+}
+export default LoginForm

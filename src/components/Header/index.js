34c93d040a6f9d197356927962da9295286260e7
vinod192954 <vvinnu590@gmail.com>
2024-08-30 11:52:36 +0530

    added media queries to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,26 @@
+import {useState} from "react"
 import Cookies from 'js-cookie'
+import { IoMdClose } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 import {withRouter} from 'react-router-dom'
 import "./index.css"
 
 const Header = (props)=>{
+    const [isShow,setShow] = useState(false)
 
     const onClickLogout=()=>{
         Cookies.remove("jwt_token")
         const {history} = props
         history.replace("/")
+    }
+
+    const onClickToChangeVisibility=()=>{
+        setShow(prevState=>!prevState)
+    }
+
+    const onClickToChangeButton=()=>{
+        setShow(false)
     }
 
     return (
@@ -26,6 +39,24 @@ const Header = (props)=>{
                     </button>
                 </ul>
             </div>
+            {isShow===false ?  ( <div className='hamburger-icon'>
+               <RxHamburgerMenu onClick={onClickToChangeVisibility} />
+            </div>) : ""}
+           
+            {isShow && ( <div className='mobile-view-links'>
+            <ul className="mobile-nav-links">
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Cart</li>
+                    <button onClick={onClickLogout} type="button">
+                        Logout
+                    </button>
+                    <button className="close-button" onClick={onClickToChangeButton}>
+                    <IoMdClose />
+                    </button>
+                </ul>
+            </div>)}
+           
         </div>
     )
 }
